592176a5ca718b4e6a91c2075d65542991f2ff6e
Ethan Rublee <ethan.rublee@gmail.com>
2011-02-05 13:46:59 -0800

    making better- bing.

--- scripts/get_ndk_toolchain_linux.sh
+++ scripts/get_ndk_toolchain_linux.sh
@@ -1,8 +1,17 @@
-destination=$HOME
+if [ -z "$1" ]
+then
+  echo "Usage: `basename $0` <path>
+  the path will be used to download and untar the android-ndk to."
+  exit $E_NOARGS
+fi
+export destination=$1
+if [ ! -e $destination ]; then
+  echo "$destination does not exist!"
+  exit 1
+fi
 ball=android-ndk-r5b-linux-x86.tar.bz2
 hash=4c0045ddc2bfd657be9d5177d0e0b7e7
-mkdir -p tmp
-cd tmp
+cd $destination
 if [ -e $BALL ]; then
   CSUM1=`md5sum $ball | grep --only-matching -m 1 '^[0-9a-f]*'`
   if [ "$CSUM1" = "$hash" ]; then
@@ -13,8 +22,12 @@ if [ -e $BALL ]; then
 else
   wget http://dl.google.com/android/ndk/$ball
 fi
+if [ ! -e  $destination/android-ndk-r5b ]; then
 echo Untarring
 tar -C $destination -xf $ball
+else
+echo Already untarred
+fi
 cd $destination/android-ndk-r5b
 NDK=`pwd`
 $NDK/build/tools/make-standalone-toolchain.sh --platform=android-5 --install-dir=$destination/android-toolchain
